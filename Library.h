c1b06765ce3cca90f183621840d5b0aa4dce4982
Alex Fang <frjalex@temple.edu>
2017-02-20 13:48:21 -0500

    finally librarize!

---
+++ Library.h
@@ -0,0 +1,23 @@
+/*
+ * Created by Alex Fang on 2/20/17.
+ * Copyright (c) 2017 Conceptual Inertia, Inc. All rights reserved.
+ *
+ * License included in project root folder.
+ * Legal Contact: Conceptual-Inertia@magikpns.com
+ */
+
+#ifndef LAN_VID_PSEUDOSTREAM_LIBRARY_H
+#define LAN_VID_PSEUDOSTREAM_LIBRARY_H
+
+#include "opencv2/opencv.hpp"
+#include "config.h"
+#include "PracticalSocket.h" // For UDPSocket and SocketException
+#include <iostream>          // For cout and cerr
+#include <cstdlib>           // For atoi()
+#define SERVER_PORT "1111"
+#define SERVER_ADDRESS "localhost"
+#define CAMERA_NUMBER 0
+#define BUF_LEN 65540 // Larger than maximum UDP packet size
+
+
+#endif //LAN_VID_PSEUDOSTREAM_LIBRARY_H
